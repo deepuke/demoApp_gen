14933750dfd6fd0187e1446e33b31c2303f6bc5d
Deepu Elayakunju <dipuke@gmail.com>
2021-04-18 21:43:52 +0530

    DB service changes added

--- public/script.js
+++ public/script.js
@@ -1,3 +1,10 @@
-jQuery(function() {
-    alert($('#input-name').val());
-});
+(async function init() {
+    const response = await fetch('http://localhost:3050/get-profile');
+    console.log("response", response);
+    const user = await response.json();
+    console.log(JSON.stringify(user));
+
+    $('#input-name').val(user.name ? user.name : 'Anna Smith');
+    $('#input-email').val(user.email ? user.email : 'anna.smith@example.com');
+    $('#input-interests').val(user.interests ? user.interests : 'coding');
+})();
--- server.js
+++ server.js
@@ -2,9 +2,13 @@ const express = require('express');
 const path = require('path');
 const fs = require('fs');
 const app = express();
+const MongoClient = require('mongodb').MongoClient;
+const bodyParser = require('body-parser');
 
 //Set the public folder
 app.use(express.static(__dirname + '/public'));
+
+app.use(bodyParser.json());
 
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'public/index.html'));
@@ -18,6 +22,45 @@ app.get('/profilePicture', (req, res) => {
     });
     res.end(img, 'binary');
 });
+
+app.get('/get-profile', (req, res) => {
+    var response = res;
+    MongoClient.connect('mongodb://admin:password@localhost:27017', (err, client) => {
+        if (err) throw err;
+
+        const db = client.db('user-account');
+        const query = { userid: 1 };
+        db.collection('users').findOne(query, (err, result) => {
+            if (err) throw err;
+
+            client.close();
+            response.send(result);
+        })
+    })
+});
+let mongoClientOptions = { useNewUrlParser: true, useUnifiedTopology: true };
+app.get('/update-profile', (req, res) => {
+    let userObj = req.body;
+    MongoClient.connect('mongodb://admin:password@localhost:27017', mongoClientOptions, function(err, client) {
+        if (err) throw err;
+
+        let db = client.db(databaseName);
+        userObj['userid'] = 1;
+
+        let myquery = { userid: 1 };
+        let newvalues = { $set: userObj };
+
+        db.collection("users").updateOne(myquery, newvalues, { upsert: true }, function(err, res) {
+            if (err) throw err;
+            client.close();
+        });
+
+    });
+    // Send response
+    res.send(userObj);
+
+});
+
 
 app.listen(3050, () => {
     console.info('Server is listening on port 3050');
